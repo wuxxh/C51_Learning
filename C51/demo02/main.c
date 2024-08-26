4429d80c5b24299ff2058c0e6692b183841da2cc
Elian Wu <elianwu@foxmail.com>
2024-08-26 16:59:57 +0800

    中断

--- C51/demo02/main.c
+++ C51/demo02/main.c
@@ -13,17 +13,30 @@ void delay_ms(unsigned int xms)   //@12MHz
     }
 }
 
+void exit1() interrupt 2
+{
+	if(key1 == 0){
+			led1 = ~led1;
+			delay_ms(1000);
+		}
+}
 
 void main(){
-	led1 = 0;
-	while(1){
+	/*while(1){
 		if(key1 == 0){
-			delay_ms(10);
+			delay_ms(30);
 			if(key1 == 0) //push
 			{
 				led1 = ~led1;
 				delay_ms(100);
 			}
 		}
+	}*/
+	PX1=1;
+	IT1 = 1; //设置外部中断源INT0的判断方式为下降沿触发
+	EX1 = 1; //允许外部中断源0
+	EA = 1; //cpu接受中断
+	while(1){
+		delay_ms(20000);
 	}
 }
